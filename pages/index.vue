05419a98b3bd4db5ba2b801d34253e7c10321a19
ian.yu <ian.yu@xuenn.com>
2021-01-19 16:31:39 +0800

    (^) rebuild project

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">nuxt-blog</h1>
+      <h1 class="title">
+        nuxt-blog
+      </h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
@@ -20,14 +22,6 @@
         >
           GitHub
         </a>
-        <a
-          href="https://github.com/ian902792"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          ian902792
-        </a>
       </div>
     </div>
   </div>
@@ -38,11 +32,6 @@ export default {}
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -53,8 +42,16 @@ export default {}
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
