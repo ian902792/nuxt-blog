8553b97af7c68cac7db07e94412651d317616e29
ian.yu <ian.yu@xuenn.com>
2021-03-02 18:53:55 +0800

    (^) test

---
+++ content/articles/my-first-blog-post.md
@@ -0,0 +1,33 @@
+---
+title: My first Blog Post title
+description: Learning how to use @nuxt/content to create a blog
+img: first-blog-post.jpg
+alt: alt
+---
+
+## This is a heading
+
+This is some more info
+
+### This is a sub heading
+
+This is some more info
+
+### This is another sub heading
+
+This is some more info
+
+## This is another heading
+
+This is some more info
+
+<div class="bg-blue-500 text-white p-4 mb-4">
+  This is HTML inside markdown that has a class of note
+</div>
+
+
+<info-box>
+  <template #info-box>
+    This is a vue component inside markdown using slots
+  </template>
+</info-box>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,8 +1,20 @@
 <template>
-  <article>
-          <h1>{{ article.title }}</h1>
-    <nuxt-content class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto" :document="article" />
-  </article>
+  <div>
+    <nav>
+      <ul>
+        <li v-for="link of article.toc" :key="link.id">
+          <NuxtLink :to="`#${link.id}`" :class="{ 'py-2': link.depth === 2, 'ml-2 pb-2': link.depth === 3 }" >{{ link.text }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <article>
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.description }}</p>
+      <p>Post last updated: {{ formatDate(article.updatedAt) }}</p>
+      <nuxt-content class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto" :document="article" />
+    </article>
+  </div>
+
 </template>
 
 <script>
@@ -11,6 +23,12 @@
       const article = await $content('articles', params.slug).fetch()
 
       return { article }
+    },
+    methods: {
+      formatDate(date) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        return new Date(date).toLocaleDateString('en', options)
+      }
     }
   }
 </script>
