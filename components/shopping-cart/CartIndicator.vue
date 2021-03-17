534ec9cccce4d2448a6a0f32d3344380e3bd1491
ian.yu <ian.yu@xuenn.com>
2021-03-17 16:09:27 +0800

    (+) Add shopping cart test page

---
+++ components/shopping-cart/CartIndicator.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="h-full flex p-2 pr-6">
+  <div class="flex flex-col justify-center">
+    <p>Subtotal: R$ {{ formatPrice(subtotal) }}</p>
+  </div>
+  <div class="relative h-full">
+    <button class="w-24 h-full text-gray-200 -mr-6"
+      @click="openCart"
+    >
+      <img class="w-10 ml-5"
+        src="~/assets/img/shopping-cart.svg"
+        alt="Carrinho"
+      >
+      <span
+        class="inline-block w-6 bg-red-500 absolute right-0 top-0 rounded-full leading-normal"
+        v-if="quantity"
+      >
+        {{ quantity }}
+      </span>
+    </button>
+  </div>
+</div>
+</template>
+
+<script>
+import priceMixin from '~/mixins/price';
+
+export default {
+  name: 'cart-indicator',
+  mixins: [priceMixin],
+  computed: {
+    quantity() {
+      return this.$store.getters.totalQuantity;
+    },
+
+    subtotal() {
+      return this.$store.getters.subtotal;
+    },
+  },
+  methods: {
+    openCart() {
+      window.scrollTo(0, 0);
+      this.$store.commit('openCart');
+    },
+  },
+};
+</script>
---
+++ components/shopping-cart/Product.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="flex flex-col items-center md:w-1/3 p-4 leading-loose">
+    <figure>
+      <img :src="imgSrc" alt="Product Image">
+      <figcaption class="text-center">{{ title }}</figcaption>
+    </figure>
+    <div class="w-12 mb-4 border-t-2 border-indigo"></div>
+    <p>
+      <small class="text-grey-darker">R$</small>
+      <span class="text-xl font-semibold">{{ formatPrice(price) }}</span>
+    </p>
+    <p class="text-grey-darker">
+      ou {{ installments }} x <small>R$</small>
+      <span class="font-semibold">
+        {{ formatPrice(installmentPrice) }}
+      </span>
+    </p>
+    <p>Quantidade:
+      <m-input-number
+        v-model="quantity"
+        :min="1"
+        :max="10"
+      />
+    </p>
+    <div class="my-2">
+      <m-button
+        class="py-2 px-4"
+        @click="addToCart"
+      >
+        Adicionar ao carrinho
+      </m-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MInputNumber from '~/components/shopping-cart/MInputNumber';
+import MButton from '~/components/shopping-cart/MButton';
+import priceMixin from '~/mixins/price';
+
+export default {
+  name: 'product',
+  mixins: [priceMixin],
+  props: [
+    'currencyFormat',
+    'id',
+    'image',
+    'installments',
+    'price',
+    'title',
+  ],
+  data() {
+    return {
+      quantity: 1,
+    };
+  },
+  computed: {
+    installmentPrice() {
+      return this.price / this.installments;
+    },
+
+    imgSrc() {
+      return `https://via.placeholder.com/220/${this.image}`;
+    },
+  },
+  methods: {
+    addToCart() {
+      const quantity = Number.parseInt(this.quantity, 10);
+      if (quantity) {
+        this.$store.commit('addToCart', {
+          productId: this.id,
+          quantity,
+        });
+      }
+    },
+  },
+  components: {
+    MInputNumber,
+    MButton,
+  },
+};
+</script>
